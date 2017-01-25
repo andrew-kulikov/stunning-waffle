de93d8652404c2cdc306f90f910bce2b3de36140
andrew-kulikov <andrew.1.kulikov@gmail.com>
2017-01-25 11:24:03 +0300

    Added new folder with small game

---
+++ year1/prog/Tortoise/FirstApp/MainTortoise/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace MainTortoise {
+    class Program {
+        private static void GraphicsWindow_KeyDown() {
+            if (GraphicsWindow.LastKey == "Up") {
+                Turtle.Angle = 0;
+            }
+            else if (GraphicsWindow.LastKey == "Down") {
+                Turtle.Angle = 180;
+            }
+            else if (GraphicsWindow.LastKey == "Right") {
+                Turtle.Angle = 90;
+            }
+            else if(GraphicsWindow.LastKey == "Left") {
+                Turtle.Angle = -90;
+            }
+        }
+        static void Main(string[] args) {
+            GraphicsWindow.KeyDown += GraphicsWindow_KeyDown;
+            Turtle.PenUp();
+            GraphicsWindow.BrushColor = "#3CF665";
+            var eat = Shapes.AddRectangle(20, 20);
+            Random rand = new Random();
+            int eatX = rand.Next() % 600;
+            int eatY = rand.Next() % 420;
+            Shapes.Move(eat, eatX, eatY);            
+            for (;;) {
+                if (Turtle.X >= eatX && Turtle.X <= eatX + 20 && Turtle.Y >= eatY
+                && Turtle.Y <= eatY + 20) {
+                    Random rand1 = new Random();
+                    eatX = rand1.Next() % 600;
+                    eatY = rand1.Next() % 420;
+                    Shapes.Move(eat, eatX, eatY);
+                    Turtle.Speed += 1;
+                }
+                Turtle.Move(10);
+                if (Turtle.X < 0) {
+                    Turtle.X = 600;
+                }
+                if (Turtle.Y < 0) {
+                    Turtle.Y = 420;
+                }
+                if (Turtle.X > 600) {
+                    Turtle.X = 0;
+                }
+                if (Turtle.Y > 420) {
+                    Turtle.Y = 0;
+                }
+            }
+            
+        }
+    }
+}
---
+++ year1/prog/Tortoise/FirstApp/Tortoise/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace Tortoise {
+    class Program {
+        static void WriteT(int x, int y) {
+            Turtle.X = x;
+            Turtle.Y = y;
+            Turtle.Angle = 0;
+            Turtle.Move(100);
+            Turtle.Turn(-90);
+            Turtle.Move(50);
+            Turtle.Turn(180);
+            Turtle.Move(100);
+        }
+        static void WriteO(int x, int y) {
+            Turtle.X = x;
+            Turtle.Y = y;
+            Turtle.Angle = 0;
+            for (int i = 0; i < 4; i++) {
+                if (i % 2 == 0) {
+                    Turtle.Move(100);
+                }
+                else {
+                    Turtle.Move(50);
+                }
+                Turtle.TurnRight();
+            }
+        }
+        static void WriteR(int x, int y) {
+            Turtle.X = x;
+            Turtle.Y = y;
+            Turtle.Angle = 0;
+            Turtle.Move(100);
+            Turtle.TurnRight();
+            for (int i = 0; i < 3; i++) {
+                Turtle.Move(50);
+                Turtle.TurnRight();
+            }
+            Turtle.Angle = 135;
+            Turtle.Move(50 * System.Math.Sqrt(2));
+        }
+        static void WriteI(int x, int y) {
+            Turtle.X = x;
+            Turtle.Y = y;
+            Turtle.Angle = 0;
+            Turtle.TurnLeft();
+            Turtle.Move(25);
+            Turtle.Turn(180);
+            Turtle.Move(50);
+            Turtle.X = x;
+            Turtle.Y = y;
+            Turtle.Angle = 0;
+            Turtle.Move(100);
+            Turtle.TurnLeft();
+            Turtle.Move(25);
+            Turtle.Turn(180);
+            Turtle.Move(50);
+        }
+        static void WriteS(int x, int y) {
+            Turtle.X = x;
+            Turtle.Y = y;
+            Turtle.Angle = 90;
+            Turtle.Move(50);
+            Turtle.Turn(-90);
+            Turtle.Move(50);
+            Turtle.TurnLeft();
+            Turtle.Move(50);
+            Turtle.TurnRight();
+            Turtle.Move(50);
+            Turtle.TurnRight();
+            Turtle.Move(50);
+        }
+        static void WriteE(int x, int y) {
+            Turtle.X = x;
+            Turtle.Y = y;
+            Turtle.Angle = 90;
+            Turtle.Move(50);
+            Turtle.X = x;
+            Turtle.Y = y;
+            Turtle.Angle = 0;
+            Turtle.Move(100);
+            Turtle.TurnRight();
+            Turtle.Move(50);
+            Turtle.X = x;
+            Turtle.Y = y - 50;
+            Turtle.Angle = 90;
+            Turtle.Move(50);
+        }
+        static void Main(string[] args) {
+            Turtle.Speed = 10;
+            WriteT(100, 150);
+            WriteO(160, 150);
+            WriteR(220, 150);
+            WriteT(330, 150);
+            WriteO(390, 150);
+            WriteI(480, 150);
+            WriteS(540, 150);
+            WriteE(305, 280);
+        }
+    }
+}
